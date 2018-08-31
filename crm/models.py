9cb01f23890e19b86f8a99757e03ce9130b5034d
Lawrence <lawrence_gandhar@yahoo.com>
2018-08-31 22:03:58 +0530

    changed model names that are colliding with django models

--- crm/models.py
+++ crm/models.py
@@ -6,10 +6,12 @@ from django.contrib.auth.models import User
 # USERTYPE MODEL
 #
 
-class Usertypes(models.Model):
+class Usertype(models.Model):
     name = models.CharField(max_length = 20, unique = True, db_index = True,)
     sub_link = models.CharField(max_length = 250, unique = True, db_index = True, blank = True, null = True,) 
     menu_template_name = models.CharField(max_length = 100, null = True, blank = True, db_index = True,)
+    use_nav_from_template_folder = models.BooleanField(default = True, db_index = True,)
+    template_folder = models.CharField(max_length = 250, blank = True, null = True, db_index = True,)
     status = models.BooleanField(default = 1, db_index = True,)
 
     def __str__(self):
@@ -17,6 +19,6 @@ class Usertypes(models.Model):
 
 class User_usertype(models.Model):
     user = models.OneToOneField(User, on_delete = models.SET_NULL, db_index = True, null = True, blank = True,)
-    usertype = models.OneToOneField(Usertypes, on_delete = models.SET_NULL, db_index = True, null = True, blank = True,)
+    usertype = models.OneToOneField(Usertype, on_delete = models.SET_NULL, db_index = True, null = True, blank = True,)
 
 
