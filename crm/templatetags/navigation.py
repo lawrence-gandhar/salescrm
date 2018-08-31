7a0d4b27858b0a7b66e77e016f179957cc2dd969
Lawrence <lawrence_gandhar@yahoo.com>
2018-09-01 00:54:32 +0530

    modified filters

--- crm/templatetags/navigation.py
+++ crm/templatetags/navigation.py
@@ -10,7 +10,7 @@ from crm.models import *
 from django.db.models import Q, When
 from django.core.exceptions import ObjectDoesNotExist
 
-from django.utils import timezone
+from django.utils import timezone, safestring
 
 # use Library
 register = template.Library()
@@ -51,14 +51,27 @@ def page_title():
 @register.simple_tag
 def current_user_link(value):
     value = int(value)
-    link = ''
+    url_link = ''
 
     try:
-        url = Usertype.objects.get(pk = value)
-        link = url.sub_link
-    except:
+        usertype = User_usertype.objects.get(user_id = value)
+        usertype_id = usertype.usertype_id
+
+        try:
+            target_link = Usertype.objects.get(pk = int(usertype_id))
+            url_link = target_link.sub_link
+
+        except User_usertype.DoesNotExist:
+            pass 
+        except TypeError: 
+            pass       
+
+    except User_usertype.DoesNotExist:
+        pass    
+    except TypeError: 
         pass
-    return link
+
+    return url_link
 
 
 #
@@ -68,15 +81,62 @@ def current_user_link(value):
 @register.filter
 def menu_template(value):
     value = int(value)
-    nav_template = settings.BASE_DIR+"/app/templates/crm/"
+    nav_template = settings.BASE_DIR+"/crm/templates/crm/"
 
     try:
-        nav_tmp = Usertype.objects.get(pk = value)
+        usertype = User_usertype.objects.get(user_id = value)
+        usertype_id = usertype.usertype_id
 
-        if nav_tmp.use_nav_from_template_folder:
-            nav_template += nav_tmp.template_folder +"/"+ nav_tmp.menu_template_name
-        else:
-            nav_template += nav_tmp.menu_template_name   
-    except:
+        try:
+            nav_tmp = Usertype.objects.get(pk = usertype_id)
+
+            if nav_tmp.use_nav_from_template_folder:
+                nav_template += nav_tmp.template_folder +"/"+ nav_tmp.menu_template_name
+            else:
+                nav_template += nav_tmp.menu_template_name
+
+        except User_usertype.DoesNotExist:
+            pass 
+        except TypeError: 
+            pass       
+
+    except User_usertype.DoesNotExist:
+        pass    
+    except TypeError: 
         pass
-    return nav_template    
+    return nav_template   
+
+
+#
+# Tag for loading css files
+#
+
+@register.simple_tag
+def load_css_files(scripts = list()):
+
+    html = ''
+
+    for script in scripts:
+        html += '<link rel="stylesheet" href="'+settings.STATIC_URL+script+'"/>'
+        
+    return safestring.mark_safe(html)
+
+
+#
+# Tag for loading javascript files
+#
+
+@register.simple_tag
+def load_javascript_files(scripts = list()):
+
+    html = '<script src="'+settings.STATIC_URL+'vendor/pacejs/pace.min.js'+'"></script>'
+    html += '<script src="'+settings.STATIC_URL+'vendor/jquery/dist/jquery.min.js'+'"></script>'
+    html += '<script src="'+settings.STATIC_URL+'vendor/bootstrap/js/bootstrap.min.js'+'"></script>'
+    html += '<script src="'+settings.STATIC_URL+'vendor/toastr/toastr.min.js'+'"></script>'
+
+    for script in scripts:
+        html += '<script src="'+settings.STATIC_URL+script+'"></script>'
+
+    html += '<script src="'+settings.STATIC_URL+'scripts/luna.js'+'"></script>'
+        
+    return safestring.mark_safe(html)
