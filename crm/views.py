12787d08b5349f394571dfe3b5c243984fb370d2
Lawrence <lawrence_gandhar@yahoo.com>
2018-08-31 22:04:39 +0530

    changes to login method

--- crm/views.py
+++ crm/views.py
@@ -24,7 +24,7 @@ from django.core.exceptions import ObjectDoesNotExist
 # Other imports
 from django.shortcuts import render, redirect
 
-#from .models import Usertype, Location, CustomUser, Department, Designation
+from .models import Usertype, User_usertype
 #from .models import Assessment_Settings, AssessmentFormOptions, AssessmentFormQuestions, AssessmentFormSection
 
 import sys, os, csv, json, datetime
@@ -33,6 +33,9 @@ import sys, os, csv, json, datetime
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+
+#
+from django.contrib import messages
 
 # Create your views here.
 
@@ -51,7 +54,32 @@ def index(request):
             
             login(request, user)
 
-            return redirect('dashboard/')
+            url_link = '/'
+
+            try:
+                usertype = User_usertype.objects.get(user_id = int(user.id))
+                usertype_id = usertype.usertype_id
+
+                try:
+                    target_link = Usertype.objects.get(pk = int(usertype_id))
+                    url_link = target_link.sub_link
+
+                except User_usertype.DoesNotExist:
+                    messages.error(request, 'Error occurred during authentication.<br/> Please Contact Administrator')
+                    return redirect('/', 'refresh')      
+                except TypeError: 
+                    messages.error(request, 'Error occurred during authentication.<br/> Please Contact Administrator')
+                    return redirect('/', 'refresh')           
+
+            except User_usertype.DoesNotExist:
+                messages.error(request, 'Error occurred during authentication.<br/> Please Contact Administrator')
+                return redirect('/', 'refresh')      
+            except TypeError: 
+                messages.error(request, 'Error occurred during authentication.<br/> Please Contact Administrator')
+                return redirect('/', 'refresh')      
+
+            return redirect(url_link + '/dashboard/')
+        messages.error(request, 'Invalid Username or Password')
     return render(request, 'crm/login.html', {})
 
 
@@ -80,5 +108,5 @@ def change_password(request):
 #*******************************************************************************    
 
 @login_required
-def dashboard(request):
+def dashboard(request,  usertype = None):
     return render(request, 'crm/dashboard.html', {})
