dbe2d3fd39a29a23714930d810c34de440c30679
Lawrence <lawrence_gandhar@yahoo.com>
2018-09-01 01:40:59 +0530

    renamed file

---
+++ crm/models-old.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+#
+# USERTYPE MODEL
+#
+
+class Usertype(models.Model):
+    name = models.CharField(max_length = 20, unique = True, db_index = True,)
+    sub_link = models.CharField(max_length = 250, unique = True, db_index = True, blank = True, null = True,) 
+    menu_template_name = models.CharField(max_length = 100, null = True, blank = True, db_index = True,)
+    use_nav_from_template_folder = models.BooleanField(default = True, db_index = True,)
+    template_folder = models.CharField(max_length = 250, blank = True, null = True, db_index = True,)
+    status = models.BooleanField(default = 1, db_index = True,)
+
+    def __str__(self):
+        return self.name.title()
+
+class User_usertype(models.Model):
+    user = models.OneToOneField(User, on_delete = models.SET_NULL, db_index = True, null = True, blank = True,)
+    usertype = models.OneToOneField(Usertype, on_delete = models.SET_NULL, db_index = True, null = True, blank = True,)
+
+
